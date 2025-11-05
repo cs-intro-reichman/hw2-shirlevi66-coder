19df7ed978f15cabe48ad503a5ee3c37603f22c7
shir levi <shirlevi66@gmail.com>
2025-11-05 19:02:56 +0200

    new

--- TimeCalc.java
+++ TimeCalc.java
@@ -1,5 +1,14 @@
 public class TimeCalc {
     public static void main(String[] args) {
         // Replace this comment with your code
+        String time = (args[0]);
+int hours = Integer.parseInt("" + time.charAt(0) + time.charAt(1) );
+int minutes = Integer.parseInt("" + time.charAt(3) + time.charAt(4) );
+int addtime = Integer.parseInt(args[1]);
+int total_minutes = (hours * 60 + minutes + addtime);
+int totalhours = (total_minutes / 60);
+int newhours = totalhours % 24; // The remainder operator % is also known as modulo
+int newminutes = total_minutes - totalhours * 60; 
+System.out.println((newhours < 10 ? "0" : "") + newhours + ":" + (newminutes < 10 ? "0" : "") + newminutes);
     }
 }
